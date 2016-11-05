3b258a20548bba747f68de7b700d1b1f191cb6e7
Brian Hogan <brianhogan@napcs.com>
2016-11-05 15:32:21 -0500

    levels

--- src/GameState.js
+++ src/GameState.js
@@ -10,6 +10,9 @@ class GameState extends Phaser.State {
 
     this.player.anchor.set(0.5);
     this.player.speed = 250;
+
+    this.playerLevel = 1;
+    this.playerXP = 0;
   }
 
   defineEnemies () {
@@ -50,13 +53,26 @@ class GameState extends Phaser.State {
     this.weapon.trackSprite(this.player, 0, 0, true);
   }
 
+  // Determines level based on current player XP
+  calculateLevel() {
+    let speed = 10 + (this.playerLevel * 10);
+    let delay = 1000 - (this.playerLevel * 10);
+
+    // don't drop below a sensible floor.
+    if (delay < 100) delay = 100;
+
+    return {
+      enemyDelay: delay,
+      enemySpeed: speed
+    }
+  }
+
+  levelUpPlayer() {
+    let step = 5;
+    this.playerLevel = Math.floor(Math.log(this.playerXP + 1, step));
+  }
+
 	create() {
-
-    this.levels = [
-      {enemyDelay: 500, enemySpeed: 30 }
-    ]
-
-    this.currentLevel = this.levels[0];
 
     this.stage.backgroundColor = '#BDC2C5';
     this.physics.startSystem(Phaser.Physics.ARCADE);
@@ -65,14 +81,15 @@ class GameState extends Phaser.State {
 		this.center = { x: this.world.centerX, y: this.game.world.centerY }
 
     let scoreStyle = { font: "16px Arial", fill: "#ff0044", align: "center" };
-    let score = this.add.text(0, 0, "0", scoreStyle);
-
+    this.score = this.add.text(0, 0, "0", scoreStyle);
 
     this.definePlayer();
     this.defineEnemies();
     this.defineKeys();
     this.defineWeapons();
 
+    // starting level
+    this.currentLevel = this.calculateLevel();
 	}
 
   handlePlayerMovement() {
@@ -108,7 +125,8 @@ class GameState extends Phaser.State {
   playerShootsEnemy(bullet, enemy) {
     enemy.kill();
     bullet.kill();
-
+    this.playerXP += 1; // TODO: get from enemy?
+    this.score.text = this.playerXP;
   }
 
   enemyHitsPlayer(player, enemy) {
@@ -116,43 +134,55 @@ class GameState extends Phaser.State {
 		this.state.start('GameOverState');
   }
 
-
   update() {
     //  Run collision
     this.game.physics.arcade.overlap(this.weapon.bullets, this.enemies, this.playerShootsEnemy, null, this);
     this.game.physics.arcade.overlap(this.enemies, this.player, this.enemyHitsPlayer, null, this);
 
     this.handlePlayerMovement();
-
     this.handleEnemies();
+
+    this.levelUpPlayer();
+    this.currentLevel = this.calculateLevel();
+  }
+
+  render() {
+    this.game.debug.start(20, 20, 'black');
+    this.game.debug.line();
+    this.game.debug.line('player xp: ' + this.playerXP);
+    this.game.debug.line('current level: ' + this.playerLevel);
+    this.game.debug.line('Enemy speed: ' + this.currentLevel.enemySpeed);
+    this.game.debug.line('Enemy delay: ' + this.currentLevel.enemyDelay);
+    this.game.debug.stop();
   }
 
   handleEnemies() {
 
-
     if (this.nextEnemyAt < this.time.now && this.enemies.countDead() > 0) {
       this.nextEnemyAt = this.time.now + this.currentLevel.enemyDelay;
       var enemy = this.enemies.getFirstExists(false);
+
       // spawn at a random location
-      //
-      let enemyX = this.rnd.integerInRange(20, 480) + this.player.x + 50;
-      let enemyY = this.rnd.integerInRange(20, 480) + this.player.y + 50;
+      let enemyX = this.rnd.integerInRange(20, 480)
+      let enemyY = this.rnd.integerInRange(20, 480)
+
+      /* adjust for player */
+
+      let willCollide = Phaser.Rectangle.intersectsRaw(this.player.getBounds(), enemyX, enemyX + enemy.width, enemyY, enemyY + enemy.height, 0);
+
+      if (willCollide) {
+        enemyX + 100;
+      }
 
       enemy.reset(enemyX, enemyY);
 
- //      enemy.body.velocity.x = this.currentLevel.enemySpeed;
-  //    enemy.body.velocity.y = this.currentLevel.enemySpeed;
-
-
     }
 
-
-
-      // hurl enemy at player
-      for(let i = 0, length = this.enemies.length; i < length; i++ ){
-        let enemy = this.enemies.children[i];
-        this.physics.arcade.moveToObject(enemy, this.player, 50);
-      }
+    // hurl enemy at player
+    for(let i = 0, length = this.enemies.length; i < length; i++ ){
+      let enemy = this.enemies.children[i];
+      this.physics.arcade.moveToObject(enemy, this.player, this.currentLevel.enemySpeed);
+    }
 
   }
 
